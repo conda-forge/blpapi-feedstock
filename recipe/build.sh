f2b994c634984c57b960d00a1fd6547cc34ab202
Adam Ehlers Nyholm Thomsen <adament@adament.net>
2023-08-09 21:19:50 +0200

    Attempt to install MacOS version from wheels

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,9 @@
 
 export BLPAPI_ROOT="."
 
-if [ -d "$BLPAPI_ROOT/Darwin" ]; then
-   echo "Copying dynamic library from python wheel"
-   ls ./blpapi
-   cp -v ./blpapi/.dylibs/lib*.so $BLPAPI_ROOT/Darwin/
-fi
-
-$PYTHON -m pip install --no-deps --ignore-installed .
-
 if [ -d "$BLPAPI_ROOT/Linux" ]; then
+   $PYTHON -m pip install --no-deps --ignore-installed .
    cp -v $BLPAPI_ROOT/Linux/lib*.so $PREFIX/lib/
 else
-   cp -v $BLPAPI_ROOT/Darwin/lib*.so $PREFIX/lib/
+   $PYTHON -m pip install --no-deps --ignore-installed --index-url=https://bcms.bloomberg.com/pip/simple $PKG_NAME==$PKG_VERSION
 fi
