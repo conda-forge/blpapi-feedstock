560802a5d26465235ac0098f76f83a9ad3253f71
Ben Moss <ben@mossity.com>
2026-03-14 17:03:38 -0400

    fix(recipe): create destination dirs before copy in Windows build

--- recipe/build-lib.bat
+++ recipe/build-lib.bat
@@ -1,5 +1,9 @@
 set SRC=%SRC_DIR%\blpapi
 
+rem Ensure destination directories exist
+if not exist "%LIBRARY_PREFIX%\bin\" mkdir "%LIBRARY_PREFIX%\bin\"
+if not exist "%LIBRARY_PREFIX%\lib\" mkdir "%LIBRARY_PREFIX%\lib\"
+
 rem Install DLLs to bin, import libs to lib
 copy /Y "%SRC%\lib\*.dll" "%LIBRARY_PREFIX%\bin\"
 if errorlevel 1 exit 1
